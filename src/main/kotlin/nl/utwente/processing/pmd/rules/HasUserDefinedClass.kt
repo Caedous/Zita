95d3b5e8add1d1a522a64c1d1541ed5b5c0bc981
Adam Fulton <adam.fulton@mq.edu.au>
2025-09-15 22:33:33 +1000

    updated HasUserDefinedClass Rule

--- src/main/kotlin/nl/utwente/processing/pmd/rules/HasUserDefinedClass.kt
+++ src/main/kotlin/nl/utwente/processing/pmd/rules/HasUserDefinedClass.kt
@@ -1,4 +1,26 @@
 package nl.utwente.processing.pmd.rules
 
-class HasUserDefinedClass {
+import net.sourceforge.pmd.lang.java.ast.ASTClassOrInterfaceDeclaration
+import net.sourceforge.pmd.lang.java.rule.AbstractJavaRule
+
+class HasUserDefinedClass: AbstractJavaRule() {
+
+    private var checkedOuter = false
+
+    override fun visit(node: ASTClassOrInterfaceDeclaration?, data: Any?): Any? {
+        if (node == null || checkedOuter || node.isNested) return super.visit(node, data)
+
+        // This is the outermost class
+        checkedOuter = true
+
+        val hasInnerClass = node.findDescendantsOfType(ASTClassOrInterfaceDeclaration::class.java)
+            .any { it != node } // Exclude the outer class itself
+
+
+        if (!hasInnerClass) {
+            addViolationWithMessage(data, node,message,0,0)
+        }
+
+        return super.visit(node, data)
+    }
 }
